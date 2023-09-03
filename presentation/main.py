b0ee6a1ca89d7e4d0ec942ba4b3e77a9251ac142
Phindile <xphindile5@gmail.com>
2023-09-03 11:17:24 +0200

    Can fetch data from frontend

--- presentation/main.py
+++ presentation/main.py
@@ -1,12 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
-@app.route("/")
-def index_page():
-    return render_template("index.html")
+components = []
+attributes = {}
+
+@app.route("/",)
+def home():
+    return render_template("add_component_tab.html")
 
 
+@app.route("/component_tab", methods=["POST", "GET"])
+def add_components():
+    if request.method=="POST":
+        component ={}
+        component_atts = []
+        name_of_component = request.form["Name_of_component"]
+        if (name_of_component!=""):
+            component["Name_of_component"] = name_of_component
+            component["Attributes"] = component_atts
+        if (request.form["submit_results"]=="Add Component"):
+            if(component["Name_of_component"]):
+                print (f'name_of_component {component["Name_of_component"]} \nWith attributes {component["Attributes"]} \nto be added in components {components}')
+                return redirect(url_for("home"))
+                print("Can't append a component of empty name")
+        elif (request.form["submit_results"]=="Add Attribute"):
+            attribute = {}
+            att_name =request.form["att_name"]
+            if(att_name!=""):
+                attribute["name"]=att_name
+                att_desc = request.form["att_description"]
+                attribute["description"]=att_desc
+                att_val = request.form["att_default_value"]
+                if(att_val==""):
+                    att_val = "NONE"
+                attribute["default_value"] = att_val
+                attributes.append(attribute)
+                component_atts.append(attribute)
+                print(f"New attribute{component_atts} to be added to component{components[0]}")
+            else:
+                print("Attribute name empty attribute not added to component")
+    else:
+        return render_template("add_component_tab.html")
 
+
+# @app.route("/add_attributes", methods= ["POST", "GET"])
+def add_attributes(form):
+
+    return "Attributes"
+
+
+@app.route("/agent_tab", methods=["POST", "GET"])
+def add_agent():
+    return ""
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
